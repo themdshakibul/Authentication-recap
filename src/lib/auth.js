bfd6ffdf7cf94ea6c5cffd1521f21a1eadb37db8
themdshakibul <mdshakibul88240@gmail.com>
2026-04-22 18:48:04 +0600

    authentic completd

---
+++ src/lib/auth.js
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+// ex:  mongobd:https://
+// env name add width process.env
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db();
+
+export const auth = betterAuth({
+  // email and password
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client,
+  }),
+});
