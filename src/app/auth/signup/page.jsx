f4432b04b53134e60dc7a4da52ceffa72cb7b0b3
themdshakibul <mdshakibul88240@gmail.com>
2026-04-23 07:14:44 +0600

    problem solved data besh connection successfull

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,107 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Check } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+
+const SignupPages = () => {
+  // async function
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const userData = Object.fromEntries(formData.entries());
+
+    console.log(userData);
+
+    const { data, error } = await authClient.signUp.email({
+      name: userData.name,
+      email: userData.email,
+      password: userData.password,
+    });
+
+    console.log("form data submited :", { data, error });
+  };
+
+  return (
+    <div className="flex items-center justify-center h-200">
+      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+        {/* name  */}
+        <TextField
+          isRequired
+          name="name"
+          validate={(value) => {
+            if (value.length < 3) {
+              return "Name must be at least 3 characters";
+            }
+            return null;
+          }}
+        >
+          <Label>Name</Label>
+          <Input placeholder="Enter Your Name" />
+          <FieldError />
+        </TextField>
+
+        {/* email */}
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input placeholder="Enter Your Email" />
+          <FieldError />
+        </TextField>
+        {/* password */}
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <Input placeholder="Enter your password" />
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default SignupPages;
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,3 +1,4 @@
+// "use server";
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
@@ -5,7 +6,7 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 // ex:  mongobd:https://
 // env name add width process.env
 const client = new MongoClient(process.env.MONGO_URI);
-const db = client.db();
+const db = client.db("better_auth");
 
 export const auth = betterAuth({
   // email and password
@@ -14,7 +15,6 @@ export const auth = betterAuth({
   },
 
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
   }),
 });
